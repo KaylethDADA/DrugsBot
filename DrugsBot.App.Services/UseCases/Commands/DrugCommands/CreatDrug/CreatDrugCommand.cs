dcc028ff7d5ed271bfef9e77db7272c4bd51541b
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:47:11 +0200

    feat: add CreatDrugCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/DrugCommands/CreatDrug/CreatDrugCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugCommands.CreatDrug
+{
+    /// <summary>
+    /// Команда для создания нового лекарства.
+    /// </summary>
+    public sealed record CreatDrugCommand(string Name, string Manufacturer, Guid CountryCodeId) : ICommand;
+}
---
+++ DrugsBot.App.Services/UseCases/Commands/DrugCommands/CreatDrug/CreateDrugCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+using DrugsBot.App.Services.Interfaces.Repositories.CountryRepositories;
+using DrugsBot.App.Services.Interfaces.Repositories.DrugRepositories;
+using DrugsBot.Domain.Entities;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugCommands.CreatDrug
+{
+    /// <summary>
+    /// Обработчик команды создания нового лекарства.
+    /// </summary>
+    public class CreateDrugCommandHandler : ICommandHandler<CreatDrugCommand>
+    {
+        private readonly IDrugWriteRepository _drugRepository;
+        private readonly ICountryReadRepository _countryRepository;
+
+        public CreateDrugCommandHandler(IDrugWriteRepository drugWriteRepository, ICountryReadRepository countryRepository)
+        {
+            _drugRepository = drugWriteRepository;
+            _countryRepository = countryRepository;
+        }
+
+        /// <summary>
+        /// Обработка команды создания нового лекарства.
+        /// </summary>
+        /// <param name="request">Команда для создания нового лекарства.</param>
+        /// <param name="cancellationToken">Токен для отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию.</returns>
+        public async Task Handle(CreatDrugCommand request, CancellationToken cancellationToken)
+        {
+            var country = await _countryRepository.GetByIdAsync(request.CountryCodeId, cancellationToken);
+
+            if (country == null)
+                throw new ArgumentException("Указанная страна не найдена.");
+
+            var drug = new Drug(request.Name, request.Manufacturer, country.Id);
+            
+            await _drugRepository.AddAsync(drug, cancellationToken);
+        }
+    }
+}
