6f62cdf430fb0373e7b1b3d246f45fb5fe984ef9
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:47:34 +0200

    feat: add DeleteDrugCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/DrugCommands/DeleteDrug/DeleteDrugCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugCommands.DeleteDrug
+{
+    /// <summary>
+    /// Команда для удаления лекарства по идентификатору.
+    /// </summary>
+    public sealed record DeleteDrugCommand(Guid Id) : ICommand;
+}
---
+++ DrugsBot.App.Services/UseCases/Commands/DrugCommands/DeleteDrug/DeleteDrugCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+using DrugsBot.App.Services.Interfaces.Repositories.DrugRepositories;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugCommands.DeleteDrug
+{
+    /// <summary>
+    /// Обработчик команды удаления лекарства.
+    /// </summary>
+    public class DeleteDrugCommandHandler : ICommandHandler<DeleteDrugCommand>
+    {
+        private readonly IDrugWriteRepository _drugRepository;
+
+        public DeleteDrugCommandHandler(IDrugWriteRepository drugRepository)
+        {
+            _drugRepository = drugRepository;
+        }
+
+        /// <summary>
+        /// Обработка команды удаления лекарства.
+        /// </summary>
+        /// <param name="request">Команда для удаления лекарства.</param>
+        /// <param name="cancellationToken">Токен для отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию.</returns>
+        public async Task Handle(DeleteDrugCommand request, CancellationToken cancellationToken)
+        {
+            var drug = await _drugRepository.ReadRepository.GetByIdAsync(request.Id, cancellationToken);
+
+            if (drug == null)
+                throw new Exception("Drug not found");
+
+            await _drugRepository.DeleteAsync(request.Id, cancellationToken);
+        }
+    }
+}
