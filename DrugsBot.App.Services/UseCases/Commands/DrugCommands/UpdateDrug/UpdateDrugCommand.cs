71b88ab513016101a149b2db7e3971ce7c2e5585
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:47:23 +0200

    feat: add UpdateDrugCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/DrugCommands/UpdateDrug/UpdateDrugCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugCommands.UpdateDrug
+{
+    /// <summary>
+    /// Команда для обновления информации о лекарстве.
+    /// </summary>
+    public sealed record UpdateDrugCommand(Guid Id, string Name, string Manufacturer, Guid CountryCodeId) : ICommand;
+}
---
+++ DrugsBot.App.Services/UseCases/Commands/DrugCommands/UpdateDrug/UpdateDrugCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+using DrugsBot.App.Services.Interfaces.Repositories.CountryRepositories;
+using DrugsBot.App.Services.Interfaces.Repositories.DrugRepositories;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugCommands.UpdateDrug
+{
+    /// <summary>
+    /// Обработчик команды обновления информации о лекарстве.
+    /// </summary>
+    public class UpdateDrugCommandHandler : ICommandHandler<UpdateDrugCommand>
+    {
+        private readonly IDrugWriteRepository _drugRepository;
+        private readonly ICountryReadRepository _countryRepository;
+
+        public UpdateDrugCommandHandler(IDrugWriteRepository drugRepository, ICountryReadRepository countryRepository)
+        {
+            _drugRepository = drugRepository;
+            _countryRepository = countryRepository;
+        }
+
+        /// <summary>
+        /// Обработка команды обновления информации о лекарстве.
+        /// </summary>
+        /// <param name="request">Команда для обновления информации о лекарстве.</param>
+        /// <param name="cancellationToken">Токен для отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию.</returns>
+        public async Task Handle(UpdateDrugCommand request, CancellationToken cancellationToken)
+        {
+            var drug = await _drugRepository.ReadRepository.GetByIdAsync(request.Id, cancellationToken);
+            var country = await _countryRepository.GetByIdAsync(request.CountryCodeId, cancellationToken);
+
+            if (drug == null)
+                throw new KeyNotFoundException($"Drug with ID {request.Id} not found.");
+            if (country == null)
+                throw new KeyNotFoundException($"Country with code {request.CountryCodeId} not found.");
+
+            drug.Update(request.Name, request.Manufacturer, country.Id);
+            await _drugRepository.UpdateAsync(drug, cancellationToken);
+        }
+    }
+}
