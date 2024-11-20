7d6f42ee5617fce25ee874894daf09cd9ba642c6
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:46:49 +0200

    feat: add DeleteCountryCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/CountryCommands/DeleteCountry/DeleteCountryCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.CountryCommands.DeleteCountry
+{
+    /// <summary>
+    /// Команда для удаления страны по идентификатору.
+    /// </summary>
+    public sealed record DeleteCountryCommand(Guid Id) : ICommand;
+}
---
+++ DrugsBot.App.Services/UseCases/Commands/CountryCommands/DeleteCountry/DeleteCountryCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+using DrugsBot.App.Services.Interfaces.Repositories.CountryRepositories;
+
+namespace DrugsBot.App.Services.UseCases.Commands.CountryCommands.DeleteCountry
+{
+    /// <summary>
+    /// Обработчик команды удаления страны.
+    /// </summary>
+    public class DeleteCountryCommandHandler : ICommandHandler<DeleteCountryCommand>
+    {
+        private readonly ICountryWrirteRepository _countryRepository;
+
+        public DeleteCountryCommandHandler(ICountryWrirteRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        /// <summary>
+        /// Обработка команды удаления страны.
+        /// </summary>
+        /// <param name="request">Команда для удаления страны.</param>
+        /// <param name="cancellationToken">Токен для отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию.</returns>
+        public async Task Handle(DeleteCountryCommand request, CancellationToken cancellationToken)
+        {
+            var country = await _countryRepository.ReadRepository.GetByIdAsync(request.Id, cancellationToken);
+
+            if (country == null)
+                throw new Exception("Country not found");
+
+            await _countryRepository.DeleteAsync(request.Id, cancellationToken);
+        }
+    }
+}
