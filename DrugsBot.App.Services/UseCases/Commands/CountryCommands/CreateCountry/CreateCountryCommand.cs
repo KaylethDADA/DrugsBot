189b5c2c80f1c1c7e9825f11d7000fb14d916477
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:46:05 +0200

    feat: add CreateCountryCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/CountryCommands/CreateCountry/CreateCountryCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.CountryCommands.CreateCountry
+{
+    /// <summary>
+    /// Команда для создания новой страны.
+    /// </summary>
+    public sealed record CreateCountryCommand(string Name, string CountryCode) : ICommand;
+}
