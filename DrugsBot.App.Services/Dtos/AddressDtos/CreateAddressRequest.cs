738fd213d0c8f3f5938b72c52388a21f8b55e3c1
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:42:17 +0200

    feat: App, adds Dtos for Address

---
+++ DrugsBot.App.Services/Dtos/AddressDtos/CreateAddressRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace DrugsBot.App.Services.Dtos.AddressDtos
+{
+    /// <summary>
+    /// DTO для запроса создания адреса.
+    /// </summary>
+    public sealed record CreateAddressRequest(string City, string Street, string House, string CountryCode);
+}
---
+++ DrugsBot.App.Services/Dtos/AddressDtos/UpdateAddressRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace DrugsBot.App.Services.Dtos.AddressDtos
+{
+    /// <summary>
+    /// DTO для запроса обнавления адреса.
+    /// </summary>
+    public sealed record UpdateAddressRequest(string City, string Street, string House, string CountryCode);
+}
