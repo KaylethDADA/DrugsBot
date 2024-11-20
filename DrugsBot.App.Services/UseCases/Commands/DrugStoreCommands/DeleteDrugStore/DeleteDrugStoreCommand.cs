dc3a6299c70d0fea4d24f162066d11f47519f6fd
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:48:22 +0200

    feat: add DeleteDrugStoreCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/DrugStoreCommands/DeleteDrugStore/DeleteDrugStoreCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugStoreCommands.DeleteDrugStore
+{
+    /// <summary>
+    /// Команда для удаления аптеки.
+    /// </summary>
+    public sealed record DeleteDrugStoreCommand(Guid Id) : ICommand;
+}
---
+++ DrugsBot.App.Services/UseCases/Commands/DrugStoreCommands/DeleteDrugStore/DeleteDrugStoreCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+using DrugsBot.App.Services.Interfaces.Repositories.DrugStoreRepositories;
+
+namespace DrugsBot.App.Services.UseCases.Commands.DrugStoreCommands.DeleteDrugStore
+{
+    /// <summary>
+    /// Обработчик команды удаления аптеки.
+    /// </summary>
+    public class DeleteDrugStoreCommandHandler : ICommandHandler<DeleteDrugStoreCommand>
+    {
+        private readonly IDrugStoreWriteRepository _drugStoreRepository;
+
+        public DeleteDrugStoreCommandHandler(IDrugStoreWriteRepository drugStoreRepository)
+        {
+            _drugStoreRepository = drugStoreRepository;
+        }
+
+        /// <summary>
+        /// Обработка команды удаления аптеки.
+        /// </summary>
+        /// <param name="request">Команда для удаления аптеки.</param>
+        /// <param name="cancellationToken">Токен для отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию.</returns>
+        public async Task Handle(DeleteDrugStoreCommand request, CancellationToken cancellationToken)
+        {
+            var drugStore = await _drugStoreRepository.ReadRepository.GetByIdAsync(request.Id, cancellationToken);
+
+            if (drugStore == null)
+                throw new KeyNotFoundException($"DrugStore with ID {request.Id} not found.");
+
+            await _drugStoreRepository.DeleteAsync(request.Id, cancellationToken);
+        }
+    }
+}
