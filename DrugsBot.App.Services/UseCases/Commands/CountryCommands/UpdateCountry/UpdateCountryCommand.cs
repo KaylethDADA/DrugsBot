6df3637035194931baefb4f7103f3b2244323e4b
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2024-11-21 01:46:37 +0200

    feat: add UpdateCountryCommand

---
+++ DrugsBot.App.Services/UseCases/Commands/CountryCommands/UpdateCountry/UpdateCountryCommand.cs
@@ -0,0 +1,9 @@
+﻿using DrugsBot.App.Services.Interfaces.CommandComponents;
+
+namespace DrugsBot.App.Services.UseCases.Commands.CountryCommands.UpdateCountry
+{
+    /// <summary>
+    /// Команда для обновления информации о стране.
+    /// </summary>
+    public sealed record UpdateCountryCommand(Guid Id, string Name, string CountryCode) : ICommand;
+}
